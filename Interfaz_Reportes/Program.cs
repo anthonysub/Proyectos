1cebe4e05407f378d009ef8a0bbed1b2c07cae68
anthony_sub <anthony@gmail.com>
2025-10-24 06:24:11 -0600

    Ejercicio 13

---
+++ Interfaz_Reportes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+// Interfaz para impresión
+public interface IImprimible
+{
+    void Imprimir();
+}
+
+// Clase Factura
+public class Factura : IImprimible
+{
+    public void Imprimir()
+    {
+        Console.WriteLine("Imprimiendo factura con detalles de compra...");
+    }
+}
+
+// Clase Reporte
+public class Reporte : IImprimible
+{
+    public void Imprimir()
+    {
+        Console.WriteLine("Imprimiendo reporte con gráficos y estadísticas...");
+    }
+}
+
+// Clase Etiqueta
+public class Etiqueta : IImprimible
+{
+    public void Imprimir()
+    {
+        Console.WriteLine("Imprimiendo etiqueta con código de barras...");
+    }
+}
